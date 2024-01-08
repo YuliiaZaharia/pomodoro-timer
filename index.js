004757184073a0cc481d4becac4c15a5d7c4ac9c
YuliiaZaharia <juliarybalchenko999@gmail.com>
2024-01-08 14:10:01 +0300

    added audio and color change effects once press buttons in JS

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ let longBreakButton = document.getElementById("longbreak");
 let startBtn = document.getElementById("btn-start");
 let reset = document.getElementById("btn-reset");
 let pause = document.getElementById("btn-pause");
+const clickSound = document.getElementById('clickSound');
+const resetSound = document.getElementById('resetSound');
+const pomoTimer = document.getElementById('pomoTimer');
+const body = document.body;
 let time = document.getElementById("time");
 let set;
 let active = "focus";
@@ -12,6 +16,64 @@ let count = 59;
 let paused = true;
 let minCount = 24;
 time.textContent = `${minCount + 1}:00`;
+
+startBtn.addEventListener('click', () => {
+  clickSound.currentTime = 0; 
+  clickSound.play();
+});
+
+pause.addEventListener('click', () => {
+  clickSound.currentTime = 0; 
+  clickSound.play();
+});
+
+reset.addEventListener('click', () => {
+  resetSound.currentTime = 0; 
+  resetSound.play();
+});
+
+focusButton.addEventListener('click', () => {
+  body.style.backgroundColor = '#ba4849'; 
+  focusButton.style.backgroundColor = '#ba4849'; 
+  longBreakButton.style.backgroundColor = '#3e3f43'; 
+  shortBreakButton.style.backgroundColor = '#3e3f43';
+});
+
+shortBreakButton.addEventListener('click', () => {
+  body.style.backgroundColor = '#397097'; 
+  shortBreakButton.style.backgroundColor = '#397097'; 
+  focusButton.style.backgroundColor = '#3e3f43';
+  longBreakButton.style.backgroundColor = '#3e3f43'; 
+});
+
+longBreakButton.addEventListener('click', () => {
+  body.style.backgroundColor = '#38848a'; 
+  longBreakButton.style.backgroundColor = '#38848a'; 
+  shortBreakButton.style.backgroundColor = '#3e3f43';
+});
+
+pomoTimer.addEventListener('click', () => {
+  Swal.fire({
+    title: 'Good luck!',
+    imageUrl: 'pomotech.webp',
+    imageWidth: 430,
+    imageHeight: 300,
+    imageAlt: 'pomodoro'
+  });
+});
+
+const pomoRelax = document.querySelector('#pomoRelax');
+pomoRelax.addEventListener('click', () => {
+  myAudio.currentTime = 0; 
+  myAudio.play();
+});
+
+const relaxButton = document.getElementById('pomoRelax');
+const audio = document.getElementById('myAudio');
+
+relaxButton.addEventListener('click', () => {
+  audio.play();
+});
 
 const appendZero = (value) => {
   value = value < 10 ? `0${value}` : value;
